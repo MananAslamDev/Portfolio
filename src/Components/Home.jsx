bcdf16c6f8cc73eebbc24ca5252e856c99441f7d
Manan Aslam <mananaslamdev@gmail.com>
2025-04-17 14:38:51 +0500

    Added Avatar on HomePage

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,20 +1,31 @@
 import Navbar from "./NavBar";
 import Button from "./Button";
+import Avatar from '../assets/Avatar.png'
 const Home = () => {
   return (
     <>
       <Navbar />
       <h3 className="HomeBgText">Developer</h3>
-      <div className="homeContent">
-        <h2>I'm a</h2>
-        <h1 className="mainTitle">
-          FRONT END
-          <br />
-          REACT
-          <br />
-          DEVELOPER<span className="dot">.</span>
-        </h1>
-        <Button text="My Recent Projects" className="ProjectsButton"></Button>
+      <div className="HomeLayout">
+        <div className="homeContent">
+          <h2>I'm a</h2>
+          <h1 className="mainTitle">
+            FRONT END
+            <br />
+            REACT
+            <br />
+            DEVELOPER<span className="dot">.</span>
+          </h1>
+          <Button text="My Recent Projects" className="ProjectsButton"></Button>
+        </div>
+        <div className="ImageContainer">
+          <div className="BorederLines"></div>
+          <img
+            src={Avatar}
+            alt="Profile"
+            className="blobImage"
+          />
+        </div>
       </div>
     </>
   );
