ac40398ef4bd5f877dc8a741bd855bfb4b0eba93
Manan Aslam <mananaslamdev@gmail.com>
2025-04-25 15:56:05 +0500

    Made home page responsive

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -3,66 +3,55 @@ import Avatar from "../assets/Avatar.png";
 import { motion, useScroll, useTransform } from "framer-motion";
 import { useRef } from "react";
 
-const imageVariants = {
-  hidden: { opacity: 0, scale: 0.9, y: 30 },
-  visible: {
-    opacity: 1,
-    scale: 1,
-    y: 0,
-    transition: {
-      duration: 1,
-      ease: "easeOut",
-    },
-  },
-};
 const Home = () => {
   const scrollRef = useRef(null);
   const { scrollY } = useScroll();
   const y = useTransform(scrollY, [0, 500], [0, 200]);
 
   return (
-    <>
-      <div ref={scrollRef} className="relative overflow-hidden">
-        <motion.h3
-          style={{ y }}
-          className="font-poppins absolute text-[14.5vw] font-black text-[#00bcd4] opacity-10 top-[50%] left-1/2 -translate-x-1/2 -translate-y-1/2 -rotate-[25deg] pointer-events-none select-none z-[1] uppercase"
-        >
-          Developer
-        </motion.h3>
+    <div ref={scrollRef} className="relative overflow-hidden min-h-screen flex items-center">
+      {/* Background text */}
+      <motion.h3
+        style={{ y }}
+        className="font-poppins absolute text-[14.5vw] font-black text-[#00bcd4] opacity-10 top-[50%] left-1/2 -translate-x-1/2 -translate-y-1/2 -rotate-[25deg] pointer-events-none select-none z-[1] uppercase"
+      >
+        Developer
+      </motion.h3>
 
-        <div className="HomeLayout">
-          <div className="lg:p-[50px] md:p-[50px] relative z-[2] text-left max-w-[800px] py-[250px] px-[50px]">
-            <h2 className="text-[4vw] font-[Josefin+Sans] font-semibold leading-[1.2] text-[rgb(163,163,163)] my-4">
-              I'm a
-            </h2>
-            <motion.h1
-              className="text-[7vw] font-[Josefin+Sans] font-extrabold leading-[1.2] text-white my-4"
-              initial={{ opacity: 0, y: 30 }}
-              animate={{ opacity: 1, y: 0 }}
-              transition={{ duration: 1 }}
-            >
-              FRONT END
-              <br />
-              REACT
-              <br />
-              DEVELOPER<span className="text-[#39bdf8]">.</span>
-            </motion.h1>
-            <Button text="My Recent Projects"></Button>
-          </div>
-          <motion.div className="absolute right-5 top-[70%] transform translate-y-[-50%] w-[350px] h-[350px] z-0 pointer-events-none">
-            <div className="border-t-8 border-b-8 border-l-8 border-[#00bcd4] lg:h-[300px] lg:w-[350px] md:h-[300px] md:w-[350px] hidden md:block"></div>
-            <motion.img
-              src={Avatar}
-              alt="Profile"
-              className="relative lg:top-[-53%] lg:left-[55%] md:left-[55%] md:top-[-53%] top-[10%] left-[75%] transform -translate-x-1/2 -translate-y-1/2 lg:h-[500px] lg:w-[350px] md:h-[500px] md:w-[350px] h-[200px] w-[150px] object-contain shadow-[0_5px_15px_rgba(0,_0,_0,_0.2)] overflow-hidden z-30"
-              initial={{ opacity: 0 }}
-              animate={{ opacity: 1 }}
-              transition={{ duration: 1 }}
-            />
-          </motion.div>
+      {/* Content layout */}
+      <div className="flex flex-col-reverse md:flex-row justify-between items-center w-full max-w-6xl mx-auto px-6 md:px-12 py-16 relative z-[2]">
+        {/* Left Text Section */}
+        <div className="text-center md:text-left max-w-xl">
+          <h2 className="text-xl text-[#a3a3a3] font-josefin mb-2">
+            I'm a
+          </h2>
+          <motion.h1
+            className="text-4xl sm:text-5xl md:text-6xl font-extrabold text-white leading-tight font-josefin mb-6"
+            initial={{ opacity: 0, y: 30 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 1 }}
+          >
+            FRONT END <br />
+            REACT <br />
+            DEVELOPER<span className="text-[#39bdf8]">.</span>
+          </motion.h1>
+          <Button text="My Recent Projects" />
+        </div>
+
+        {/* Right Image Section */}
+        <div className="relative mb-12 md:mb-0 w-full md:w-auto flex justify-center md:justify-end">
+          <div className="hidden md:block absolute top-33 left-20 border-t-8 border-b-8 border-l-8 border-[#00bcd4] h-[300px] w-[350px]"></div>
+          <motion.img
+            src={Avatar}
+            alt="Profile"
+            className="relative z-10 md:left-20 lg:left-20 h-[200px] w-[150px] sm:h-[250px] sm:w-[180px] md:h-[500px] md:w-[350px] object-contain shadow-lg"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 1 }}
+          />
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
